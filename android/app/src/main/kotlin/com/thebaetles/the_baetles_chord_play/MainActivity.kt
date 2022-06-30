30a4ba66937c56a37bff2a252c1660e611847a02
CaChiJ <chj7239@gmail.com>
2022-06-30 16:45:18 +0900

    feat: 피치 트래커

--- android/app/src/main/kotlin/com/thebaetles/the_baetles_chord_play/MainActivity.kt
+++ android/app/src/main/kotlin/com/thebaetles/the_baetles_chord_play/MainActivity.kt
@@ -1,6 +1,67 @@
 package com.thebaetles.the_baetles_chord_play
 
+import android.Manifest
+import android.content.Intent
+import androidx.annotation.NonNull
+import androidx.activity.result.registerForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ComponentActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    private val PERMISSION_REQUEST_CODE = 1001;
+
+    private val METHOD_CHANNEL = "com.example.baetles/chord-detection"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            var permissions = arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            )
+            ActivityCompat.requestPermissions(this, permissions, 100)
+        }
+    }
+
+    // flutter method channel 연결
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, METHOD_CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "startDetectingChord") {
+                // permission check
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.RECORD_AUDIO
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    result.error("403", "RECORD_AUDIO permission not granted", null);
+                }
+
+                // 음 판별 시작
+                val pitchTracker : PitchTracker = PitchTracker(this)
+                pitchTracker.start()
+
+                result.success(1234)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,76 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:the_baetles_chord_play/widgets/atoms/VideoThumbnail.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text("Chord Play"),
+          backgroundColor: Color.fromARGB(255, 28, 28, 30),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            VideoThumbnail(),
+            ChordDetectionTest(),
+          ]
         ),
       ),
     );
+  }
+}
+
+class ChordDetectionTest extends StatefulWidget {
+  const ChordDetectionTest({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ChordDetectionTestState();
+}
+
+class _ChordDetectionTestState extends State<ChordDetectionTest> {
+  static const platform = MethodChannel('com.example.baetles/chord-detection');
+
+  String _lastPlayedChord = "Unknown chord.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [ElevatedButton(
+            onPressed: _startDetectingChord,
+            child: Text("음 판별 시작")
+        ),
+        Text(_lastPlayedChord),
+      ]
+    );
+  }
+
+  Future<void> _startDetectingChord() async {
+    String chord;
+
+    try {
+      final int result = await platform.invokeMethod('startDetectingChord');
+      chord = 'Played chord is $result';
+    } on PlatformException catch (e) {
+      chord = "Failed to get played chord: '${e.message}'.";
+    }
+
+    setState(() {
+      // 결과 저장
+      _lastPlayedChord = chord;
+    });
   }
 }
