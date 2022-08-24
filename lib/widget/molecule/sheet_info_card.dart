8ba5065d9abf45dc0a68ee1da900b43beb11e085
CaChiJ <chj7239@gmail.com>
2022-08-24 22:54:35 +0900

    chore : dummy 이미지 변경

--- lib/widget/molecule/sheet_info_card.dart
+++ lib/widget/molecule/sheet_info_card.dart
@@ -34,8 +34,8 @@ class SheetInfoCard extends StatelessWidget {
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(4),
-              child: Image.network(
-                "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+              child: Image.asset(
+                "assets/images/img_sheet_cover_1.jpg",
                 width: 90,
                 height: 90,
                 fit: BoxFit.cover,
@@ -87,8 +87,7 @@ class SheetInfoCard extends StatelessWidget {
                       Container(
                         child: UserInfoView(
                           userNickname: "baetles",
-                          userProfileImagePath:
-                              "https://avatars.githubusercontent.com/u/40628765?v=4",
+                          userProfileImage: Image.asset("assets/icons/ic_robot.png"),
                         ),
                       ),
                       LikeCount(
